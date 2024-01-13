23d870565200d732e2c6f1cead61781bd5ca352d
Marcelo Salvatori <marcelo1302a@gmail.com>
2024-01-13 11:44:40 -0300

    fix: Mudar variável restaurantes para um atributo da classe Restaurante

--- Modelos/restaurantes.py
+++ Modelos/restaurantes.py
@@ -1,16 +1,18 @@
-restaurantes = []
+
 class Restaurante: 
+    restaurantes = []
+
     def __init__(self, nome, categoria): 
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
-        restaurantes.append(self)
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
-        for restaurante in restaurantes:
+        for restaurante in Restaurante.restaurantes:
             print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 restaurante_praca = Restaurante('Praça','Gourmet')
