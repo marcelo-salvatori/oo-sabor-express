2903fc197b9f572c13106f21c85052f4de08b511
Marcelo Salvatori <marcelo1302a@gmail.com>
2024-01-13 10:57:57 -0300

    feat: Adicionar método listar_restaurantes na clase Restaurantes

--- Modelos/restaurantes.py
+++ Modelos/restaurantes.py
@@ -1,8 +1,18 @@
+restaurantes = []
 class Restaurante: 
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria): 
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        restaurantes.append(self)
 
-restaurante_praca = Restaurante()
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-print(restaurante_praca)
+restaurante_praca = Restaurante('Praça','Gourmet')
+
+Restaurante.listar_restaurantes()
