f5e99e515f62e3122fdac38c35ada0b213c87daa
Marcelo Salvatori <marcelo1302a@gmail.com>
2024-01-16 10:07:01 -0300

    feat: Implementar nota_valida()
    refactor: Modificar receber_nota

--- Modelos/restaurantes.py
+++ Modelos/restaurantes.py
@@ -3,19 +3,18 @@ from Modelos.avaliacao import Avaliacao
 class Restaurante: 
     restaurantes = []
 
-    def __init__(self, nome, categoria): 
+    def __init__(self, nome, categoria) -> None: 
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
-
         Restaurante.restaurantes.append(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self._nome} | {self._categoria}'
     
     @classmethod
-    def listar_restaurantes(cls):
+    def listar_restaurantes(cls) -> None:
         cabecalho = f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | Status'
         linha = f'-' * len(cabecalho)
         print(cabecalho)
@@ -24,20 +23,29 @@ class Restaurante:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_notas).ljust(25)} | {restaurante.ativo}')
     
     @property
-    def ativo(self):
+    def ativo(self) -> str:
         return f'✅' if self._ativo else '❌'
     
-    def alternar_status(self):
+    def alternar_status(self) -> None:
         self._ativo = not self._ativo
     
-    def receber_nota(self, cliente, nota):
+    def receber_nota(self, cliente, nota) -> None:
         avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        if self.nota_valida(nota) and self._ativo:
+            self._avaliacao.append(avaliacao)
+        else:
+            print('A nota digitada é inválida ou o restaurante está inativo. Tente novamente...')
+    
+    def nota_valida(self, nota) -> bool:
+        if 0 < nota <=5:
+            return True
+        else:
+            return False
     
     @property
     def media_notas(self):
         if not self._avaliacao:
-            return 0
+            return '-'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
     
--- app.py
+++ app.py
@@ -4,11 +4,9 @@ restaurante_praca = Restaurante('Praça','Gourmet')
 restaurante_pinda = Restaurante('Pindamanhagaba', 'Regional')
 restaurante_pinda.alternar_status()
 restaurante_pinda.receber_nota('Marcelo', 10)
-restaurante_pinda.receber_nota('Rai', 8)
-restaurante_pinda.receber_nota('Rai', 2)
-
-
-
+restaurante_pinda.receber_nota('Wilder', 5)
+restaurante_pinda.receber_nota('Rai', 3)
+restaurante_praca.receber_nota('Manoel', 2)
 
 def main():
     Restaurante.listar_restaurantes()
